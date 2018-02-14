218ac96bca7c0ba3555768738667c1ffcdb43ad7
Erick Lui <erickjin.lui@gmail.com>
2018-02-13 18:34:25 -0800

    imported react-router-dom and setup test routes

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+// BrowserRouter decides what to do based on change on URL
+  // I want react-router to look at the URL when deciding on displaying components
+// Route component is to provide configuration for URL like...
+  // If I see this URL, show this component
+
+import App from './components/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+class Hello extends React.Component {
+  render() { return <div>Hello!</div> }
+}
+
+class Goodbye extends React.Component {
+  render() { return <div>Goodbye!</div> }
+}
+
+render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <Route path="/hello" component={Hello} />
+        <Route path="/goodbye" component={Goodbye} />
+      </div>
+    </BrowserRouter>
+  </Provider>
+  , document.querySelector('.container'));
