c71ea6617c217c1640d2afef9b66a601dcdf7784
Erick Lui <erickjin.lui@gmail.com>
2018-02-14 14:00:09 -0800

    displayed posts in PostIndex

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
@@ -10,16 +11,32 @@ class PostIndex extends Component {
     this.props.fetchPosts();
   }
 
+  renderPosts() {
+    // need to convert object to an array of li elements using lodash
+    return _.map(this.props.posts, post => {
+      return (
+        <li className='list-group-item' key={post.id}>{post.title}</li>
+      );
+    });
+  }
+
   render() {
     return (
       <div>
-        Posts Index
+        <h3>Posts</h3>
+        <ul className='list-group'>
+          {this.renderPosts()}
+        </ul>
       </div>
     );
   }
 }
 
+function mapStateToProps(state) {
+  return { posts: state.posts };
+}
+
 // before we used to pass in mapDispatchToProps as 2nd param, here's a shortcut
 // this does exactly the same as...
 // export default connect (null, mapDispatchToProps)(PostIndex);
-export default connect(null, { fetchPosts })(PostIndex);
+export default connect(mapStateToProps, { fetchPosts })(PostIndex);
