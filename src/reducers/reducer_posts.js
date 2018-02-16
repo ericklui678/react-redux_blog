3a083ace74d32d0af362e1a9d8a90d9c177a4077
Erick Lui <erickjin.lui@gmail.com>
2018-02-16 15:19:36 -0800

    delete action now updates application state

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,11 +1,16 @@
 import _ from 'lodash';
-import { FETCH_POSTS, FETCH_POST } from '../actions';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 
 export default function (state = {}, action) {
   switch (action.type) {
+    case DELETE_POST:
+    // look at state object, if it has the key of the post id, remove from object
+    // and return new state object
+      return _.omit(state, action.payload)
     case FETCH_POSTS:
       // will use lodash to convert array of objects payload into object
       // where posts id is the key for each object
+      console.log(state);
       return _.mapKeys(action.payload.data, 'id');
     case FETCH_POST:
       // ES5 way
